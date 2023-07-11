5ce09544564cd2ffc999bb8ee27fff5de011c25c
vm2 <avantonov88@yandex.ru>
2023-07-11 22:18:37 +0300

    6 sem

---
+++ Game/src/Units/Names.java
@@ -0,0 +1,6 @@
+package Units;
+
+public enum Names {
+    Ivan, Nikolai, Mihail, Elena, Petr, Dmitry, Alex, Den, Kol, Mal, Kil, Nagibator, Konstrntin, Pankrat, Saveli, Solovei
+    , Parampampam;
+}
